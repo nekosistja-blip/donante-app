6200c6e52305ac0a113587aabbe16347e8d9694f
nekosistja-blip <nekosistja@gmail.com>
2026-03-23 16:13:01 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import sys
 import streamlit as st
-st.write(f"Versión de Python: {sys.version}")
+
+# Mostrar la versión de Python
+st.write(f"Versión de Python en Streamlit Cloud: {sys.version}")
 # Cargar Excel
 df = pd.read_excel('GRUPO SANGRE.xlsx', sheet_name=None)
 vamDonante = df['vamDonante']
